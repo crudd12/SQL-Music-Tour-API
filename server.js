9fffb5cdc1a478814cd8c2447650db5449ed274f
crudd12 <crudd12@gmail.com>
2024-04-30 19:02:41 -0700

    connected to Postgres database

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 // DEPENDENCIES
 const express = require('express')
+const { Sequelize } = require('sequelize')
 const app = express()
+
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+// SEQUELIZE CONNECTION
+const sequelize = new Sequelize(process.env.PG_URI)
+
+try {
+    sequelize.authenticate()
+    console.log(`Connected with Sequelize at ${process.env.PG_URI}`)
+} catch (err) {
+    console.log(`Unable to connect to PG: ${err}`)
+}
 
 // ROOT
 app.get('/', (req, res) => {
@@ -18,3 +30,5 @@ app.get('/', (req, res) => {
 app.listen(process.env.PORT, () => {
     console.log(`🎸 Rockin' on port: ${process.env.PORT}`)
 })
+
+
